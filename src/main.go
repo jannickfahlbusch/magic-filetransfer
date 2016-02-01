7971bc33a9bc76f7056c3ef52d607043848b3650
Jannick Fahlbusch <git@jf-projects.de>
2016-02-01 11:54:51 +0100

    Add some CLI-flags

--- src/main.go
+++ src/main.go
@@ -13,10 +13,27 @@ var (
 	fileName        = flag.String("fileName", "", "Name of the file to transmit")
 	outputDirectory = flag.String("outputDir", "", "Directory to save the retrieved file")
 	outputFileName  = flag.String("outputName", "", "Save the recieved file under a different name")
+	usage           = flag.Bool("usage", false, "Print the usage for magic-filetransfer and exit")
+	help            = flag.Bool("help", false, "Print the usage for magic-filetransfer and exit - alias for '-usage'")
+	version         = flag.Bool("version", false, "Print the version for magic-filetransfer")
 )
 
 func main() {
 	flag.Parse()
+
+	//Check, if the user wants to see the usage of mft
+	if *usage || *help {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if *version {
+		fmt.Printf("----- magic-filetransfer (mft) -----\n")
+		fmt.Printf("Jannick Fahlbusch <git@jf-projects.de>\n")
+		fmt.Printf("Version 0.1-alpha\n")
+
+		os.Exit(0)
+	}
 
 	channel := make(chan net.IP)
 	if *fileName == "" {
