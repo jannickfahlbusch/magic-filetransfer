7e592fca5c602a150c82c61f75770cb2ac73b217
Jannick Fahlbusch <git@jf-projects.de>
2016-02-20 20:40:30 +0100

    Put the definition of all flags in a seperate file

--- src/main.go
+++ src/main.go
@@ -9,16 +9,6 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-var (
-	fileName        = kingpin.Flag("fileName", "Name of the file to transmit").Short('f').ExistingFile()
-	outputDirectory = kingpin.Flag("outputDir", "Directory to save the retrieved file in").Short('o').ExistingDir()
-	outputFileName  = kingpin.Flag("outputName", "Save the recieved file under a different name").String()
-	usage           = kingpin.Flag("usage", "Print the usage for magic-filetransfer and exit").Short('u').Short('h').Bool()
-	version         = kingpin.Flag("version", "Print the version for magic-filetransfer").Short('v').Bool()
-	client          = kingpin.Flag("client", "IP-Address of the client you want to send the file to").Short('c').IP()
-	server          = kingpin.Flag("server", "IP-Address of the server you want to recieve the file from").Short('s').IP()
-)
-
 func main() {
 	kingpin.Parse()
 
